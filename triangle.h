406f3ee760a59dd11041259586d798ab4e42efd7
Sofya Shveda <SOFIIA.SHVEDA@lnu.edu.ua>
2026-03-30 21:56:52 +0300

    triangle.h

--- triangle.h
+++ triangle.h
@@ -1,21 +1,21 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
-struct Point
-{
-    double x;
-    double y;
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Point {
+    double x, y;
 };
 
-double distance(Point A, Point B);
+double dist(Point A, Point B);
+double heron(Point A, Point B, Point C);
+double gauss(Point A, Point B, Point C);
+double crossProduct(Point A, Point B, Point C);
 
-double heronArea(Point A, Point B, Point C);
-
-double gaussArea(Point A, Point B, Point C);
-
-bool degenerate(Point A, Point B, Point C);
-
-double cross(Point A, Point B, Point C);
+bool isDegenerate(Point A, Point B, Point C);
+bool onSegment(Point A, Point B, Point P);
 
 int checkPoint(Point A, Point B, Point C, Point P);
 
