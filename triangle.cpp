a081bd8fd712e2fd19cec5bb9b502c421f76b3ea
Sofya Shveda <SOFIIA.SHVEDA@lnu.edu.ua>
2026-03-30 21:57:56 +0300

    triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -1,64 +1,71 @@
 #include "triangle.h"
-#include <cmath>
 
+double eps = 1e-9;
 
-double distance(Point A, Point B)
-{
-    return sqrt((A.x - B.x)*(A.x - B.x) +
-                (A.y - B.y)*(A.y - B.y));
+double dist(Point A, Point B) {
+    return sqrt((B.x - A.x)*(B.x - A.x) +
+                (B.y - A.y)*(B.y - A.y));
 }
 
-
-double heronArea(Point A, Point B, Point C)
-{
-    double a = distance(A,B);
-    double b = distance(B,C);
-    double c = distance(C,A);
+double heron(Point A, Point B, Point C) {
+    double a = dist(A, B);
+    double b = dist(B, C);
+    double c = dist(C, A);
 
     double p = (a + b + c) / 2;
+    double s = p * (p - a) * (p - b) * (p - c);
 
-    return sqrt(p*(p-a)*(p-b)*(p-c));
+    if (s < 0) s = 0;
+
+    return sqrt(s);
 }
 
+double gauss(Point A, Point B, Point C) {
+    double s = A.x*(B.y - C.y) +
+               B.x*(C.y - A.y) +
+               C.x*(A.y - B.y);
 
-double gaussArea(Point A, Point B, Point C)
-{
-    return 0.5 * (A.x*(B.y - C.y) +
-                  B.x*(C.y - A.y) +
-                  C.x*(A.y - B.y));
+    if (s < 0) s = -s;
+
+    return s / 2;
 }
 
-
-bool degenerate(Point A, Point B, Point C)
-{
-    double S = heronArea(A,B,C);
-
-    if (fabs(S) < 1e-9)
-        return true;
-    else
-        return false;
-}
-
-
-double cross(Point A, Point B, Point C)
-{
+double crossProduct(Point A, Point B, Point C) {
     return (B.x - A.x)*(C.y - A.y) -
            (B.y - A.y)*(C.x - A.x);
 }
 
+bool isDegenerate(Point A, Point B, Point C) {
+    return gauss(A, B, C) < eps;
+}
 
-int checkPoint(Point A, Point B, Point C, Point P)
-{
-    double c1 = cross(A,B,P);
-    double c2 = cross(B,C,P);
-    double c3 = cross(C,A,P);
 
-    if((c1 > 0 && c2 > 0 && c3 > 0) ||
-       (c1 < 0 && c2 < 0 && c3 < 0))
+
+bool onSegment(Point A, Point B, Point P) {
+    if (P.x >= min(A.x, B.x) - eps &&
+        P.x <= max(A.x, B.x) + eps &&
+        P.y >= min(A.y, B.y) - eps &&
+        P.y <= max(A.y, B.y) + eps)
+        return true;
+
+    return false;
+}
+
+int checkPoint(Point A, Point B, Point C, Point P) {
+    double s1 = crossProduct(A, B, P);
+    double s2 = crossProduct(B, C, P);
+    double s3 = crossProduct(C, A, P);
+
+    
+    if (abs(s1) < eps && onSegment(A, B, P)) return 2;
+    if (abs(s2) < eps && onSegment(B, C, P)) return 2;
+    if (abs(s3) < eps && onSegment(C, A, P)) return 2;
+
+    
+
+    if ((s1 > 0 && s2 > 0 && s3 > 0) ||
+        (s1 < 0 && s2 < 0 && s3 < 0))
         return 1;
 
-    if(c1 == 0 || c2 == 0 || c3 == 0)
-        return 0;
-
-    return -1;
+    return 0;
 }
